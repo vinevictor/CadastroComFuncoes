eca43a7183bb8735d019b1380a84f617e3497da8
Vinicius Victor <vine.victordev@gmail.com>
2022-11-08 20:15:50 -0300

    Função Cadastrar Usuario

---
+++ script.js
@@ -0,0 +1,7 @@
+let nomes = [], senhas = []
+
+function CadastroUsuario() {
+    nomes.push(prompt("Digite um nome:"))
+    senhas.push(prompt("Digite uma senha:"))
+    console.log("Usuario Cadastrado.")
+}
