34d79b4501a9f81aa3ddd375c9a5da8a5ad094cb
Vinicius Victor <vine.victordev@gmail.com>
2022-11-08 20:23:48 -0300

    Função Opções

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 let nomes = [], senhas = []
+let continuar = true;
 
 function CadastroUsuario() {
     nomes.push(prompt("Digite um nome:"))
     senhas.push(prompt("Digite uma senha:"))
     console.log("Usuario Cadastrado.")
+}
+
+function Opcoes() {
+    let opcao = prompt("Digite 1 => Cadastro | 2 => Login | 3 => Excluir Usuario | 4 => Encerrar Programa.")
+    switch (opcao) {
+        case 1:
+            CadastroUsuario();
+            break;
+        case 2:
+            Login();
+            break;
+        case 3:
+            ExcluirUsuario();
+            break;
+        case 4:
+            continuar = false;
+            break;
+    }
+}
+
+while (continuar) {
+    Opcoes();
 }
