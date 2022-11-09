eeccdc9b3b1b7e3a16658d437302c467eeb3acb8
Vinicius Victor <vine.victordev@gmail.com>
2022-11-08 21:34:52 -0300

    Função Excluir Usuarios

--- script.js
+++ script.js
@@ -10,27 +10,36 @@ function CadastroUsuario() {
 function Opcoes() {
     let opcao = prompt("Digite 1 => Cadastro | 2 => Login | 3 => Excluir Usuario | 4 => Encerrar Programa.")
     switch (opcao) {
-        case 1:
+        case "1":
             CadastroUsuario();
             break;
-        case 2:
-            Login();
+        case "2":
+            let nome = prompt("Digite o Usuario:")
+            let senha = prompt("Digite a senha:")
+            console.log(Login(nome, senha))
             break;
-        case 3:
-            ExcluirUsuario();
+        case "3":
+            let usuario = prompt("Digite o Usuario que deseja excluir:")
+            ExcluirUsuario(usuario);
             break;
-        case 4:
+        case "4":
             continuar = false;
+            break;
+        default:
+            console.log("Opção Inválida")
             break;
     }
 }
 
+
 function Login(nome, senha) {
     let loginVerdadeiro = nomes.includes(nome)
     let senhaVerdadeira = senha.includes(senha)
+
     if (loginVerdadeiro && senhaVerdadeira) {
         let nomeIndex = nomes.indexOf(nome)
-        let senhaIndex = senha.indexOf(senha)
+        let senhaIndex = senhas.indexOf(senha)
+
         if (senhaIndex == nomeIndex) {
             return true
 
@@ -41,6 +50,35 @@ function Login(nome, senha) {
     } else {
         return false
     }
+}
+
+function ExcluirUsuario(usuario) {
+    let usuarioExiste = nomes.includes(usuario)
+    let nomesSup = []
+    let senhasSup = []
+    if (usuarioExiste) {
+        let index = nomes.indexOf(usuario)
+        nomes[index] = 0
+        senhas[index] = 0
+        nomes.forEach(x => {
+            if (x == 0) {
+
+            } else {
+                nomesSup.push(x)
+            }
+        })
+        senhas.forEach(x => {
+            if (x == 0) {
+
+            } else {
+                senhasSup.push(x)
+            }
+        })
+    } else {
+        console.log("Usuario não encontrado")
+    }
+    nomes = nomesSup
+    senhas = senhasSup
 }
 
 
